dbc3dc0c381b08b9661e6c568a8b1f4638a902be
Lior Oliel <Lior.Oliel20.github@gmail.com>
2025-03-17 20:05:11 +0200

    updates to post model

--- src/models/posts_model.ts
+++ src/models/posts_model.ts
@@ -1,20 +1,56 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-export interface iPost {
-  title: String,
-  content: String,
-  owner: String,
+export interface iPost extends Document {
+  title: string;
+  owner: string;
+  content: string;
+  date: Date;
+  location: string;
+  participantsIds?: string[]; 
+  img?: string;
+  likes_number?: number;
+  comments_number?: number;
 }
 
-const postSchema = new mongoose.Schema<iPost>({
+const postSchema = new Schema<iPost>({
   title: {
     type: String,
     required: true,
   },
-  content: String,
+  img: {
+    type: String,
+    default: "", 
+  },
   owner: {
     type: String,
+    ref: "users",
     required: true,
+  },
+  participantsIds: [
+    {
+      type: String,
+      ref: "users",
+    },
+  ],
+  content: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  likes_number: {
+    type: Number,
+    default: 0,
+  },
+  comments_number: {
+    type: Number,
+    default: 0,
   },
 });
 
